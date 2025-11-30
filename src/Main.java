d87954d1177167aa82fcfd770d85c327f70ea8e4
Hyouka45 <khewang212@gmail.com>
2025-11-30 18:48:24 +0545

    New DSA

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
 public class Main{
-    public static void main(String[] args){
-        Stack stack = new Stack(5);
-        stack.display();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        stack.push(50);
-        stack.push(60);
-        stack.push(70);stack.display();
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        stack.pop();
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello DSA");
     }
 }
