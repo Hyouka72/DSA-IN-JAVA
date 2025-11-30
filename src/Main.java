41273eed5dbb0aed9cdc4a0e88b4370af532b99f
Hyouka45 <khewang212@gmail.com>
2025-11-30 20:28:00 +0545

    single linked list done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello DSA");
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        list.display();
     }
 }
